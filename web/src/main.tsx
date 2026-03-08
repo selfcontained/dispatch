8162bb4ac8a0cda34c68556e521eee7574a1be76
Brad Harris <bmharris@gmail.com>
2026-03-08 11:25:07 -0600

    Add installable PWA support for web app

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import { App } from "./App";
 import "./index.css";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
